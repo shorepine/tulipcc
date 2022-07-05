ca20fd50675259d5a10ebc3618ab456aaae837ec
Brian Whitman <brian@variogr.am>
2022-07-05 08:09:49 -0400

    dist

--- ports/macos/package.sh
+++ ports/macos/package.sh
@@ -10,21 +10,22 @@ make WHICH_ARCH=x86_64
 lipo -create -output tulip tulip.x86_64 tulip.arm64
 rm tulip.x86_64
 rm tulip.arm64
+mkdir -p dist
 
 # Now make the app bundle
-rm -rf Tulip\ CC.app
-mkdir -p Tulip\ CC.app/Contents/{MacOS,Resources,Frameworks,libs}
-cp tulip Tulip\ CC.app/Contents/MacOS
-cp Info.plist Tulip\ CC.app/Contents/
-cp -rf ../../tulip_home Tulip\ CC.app/Contents/Resources/
-cp -a SDL2.framework Tulip\ CC.app/Contents/Frameworks/
-cp libs/libsoundio.2.0.0.dylib Tulip\ CC.app/Contents/libs/
-install_name_tool -add_rpath @executable_path/../Frameworks Tulip\ CC.app/Contents/MacOS/tulip
-install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libsoundio/lib/libsoundio.2.dylib" "@executable_path/../libs/libsoundio.2.0.0.dylib" "Tulip CC.app/Contents/MacOS/tulip"
-cp tulip.icns Tulip\ CC.app/Contents/Resources/
-codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" Tulip\ CC.app/Contents/libs/libsoundio.2.0.0.dylib -f
-codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" Tulip\ CC.app/Contents/Frameworks/SDL2.framework/Versions/A/SDL2 -f
-codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" Tulip\ CC.app/Contents/MacOS/tulip -f
+rm -rf dist/Tulip\ CC.app
+mkdir -p dist/Tulip\ CC.app/Contents/{MacOS,Resources,Frameworks,libs}
+cp tulip dist/Tulip\ CC.app/Contents/MacOS
+cp Info.plist dist/Tulip\ CC.app/Contents/
+cp -rf ../../tulip_home dist/Tulip\ CC.app/Contents/Resources/
+cp -a SDL2.framework dist/Tulip\ CC.app/Contents/Frameworks/
+cp libs/libsoundio.2.0.0.dylib dist/Tulip\ CC.app/Contents/libs/
+install_name_tool -add_rpath @executable_path/../Frameworks dist/Tulip\ CC.app/Contents/MacOS/tulip
+install_name_tool -change "@@HOMEBREW_PREFIX@@/opt/libsoundio/lib/libsoundio.2.dylib" "@executable_path/../libs/libsoundio.2.0.0.dylib" "dist/Tulip CC.app/Contents/MacOS/tulip"
+cp tulip.icns dist/Tulip\ CC.app/Contents/Resources/
+codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ CC.app/Contents/libs/libsoundio.2.0.0.dylib -f
+codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ CC.app/Contents/Frameworks/SDL2.framework/Versions/A/SDL2 -f
+codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ CC.app/Contents/MacOS/tulip -f
 
 # I then run "AppWrapper 4.0" to notarize the package. I would love to convert that into a command line tool, i believe xcrun notarytool
 # TODO... 
