5fd55ef7e855b7267d1577c5b6d462e0c2126790
Brian Whitman <brian@variogr.am>
2022-09-14 16:46:25 -0700

    startup

--- tulip_home/ex/startup.py
+++ tulip_home/ex/startup.py
@@ -1,27 +1,23 @@
 import time, music, random
-tulip.bg_clear(255)
-tulip.bg_png("g/tulip3.png",384,128)
+tulip.bg_clear()
+lx = 310
+ly = 75
+tulip.bg_png("tulipbw.png",lx,ly)
 
 chord = music.Chord("F:min7")
-for j in range(1):
-    patch = 4
-    ratio = 0.1
-    tic = alles.millis()
+patch = 4
+ratio = 0.1
+tic = alles.millis()
     
-    for i,note in enumerate(chord.midinotes()[:3]):
-        # note on
-        alles.send(wave=alles.ALGO,osc=i*9,note=note,vel=0.25,patch=patch,ratio=ratio,timestamp=tic+(i*200))
-        # note off
-        alles.send(osc=i*9, vel=0, timestamp=tic+1500)
-    #print(str(patch))
-    #time.sleep(3.5)
-    #alles.reset()    
+for i,note in enumerate(chord.midinotes()):
+    alles.send(wave=alles.ALGO,osc=i*9,note=note,vel=0.25,patch=patch,ratio=ratio,timestamp=tic+(i*200))
+    alles.send(osc=i*9, vel=0, timestamp=tic+1500)
 
-ct = 150
-for c in range(ct):
-    swell = ct - 1 - c
-    for y in range(600):
-        tulip.bg_scroll_x_offset(y, random.randint(-swell,swell))
+for c in range(200):
+    c = [random.randint(1,255),random.randint(1,255),random.randint(1,255)]
+    tulip.fill(lx+75,ly+200, c[0])
+    tulip.fill(lx+175,ly+200,c[1])
+    tulip.fill(lx+320,ly+200,c[2])
     time.sleep(0.01)
 
 time.sleep(0.5)
