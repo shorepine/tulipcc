6688784f1ba270ff05a2fd13b29927db5e80c534
Dan Ellis <dpwe@ee.columbia.edu>
2023-03-28 07:49:38 -0400

    alles.py: Remove stray whitespace.

--- extmod/tulip/py/alles.py
+++ extmod/tulip/py/alles.py
@@ -73,7 +73,7 @@ def trunc(number):
 # Construct an AMY message
 def message(osc=0, wave=-1, patch=-1, note=-1, vel=-1, amp=-1, freq=-1, duty=-1, feedback=-1, timestamp=None, reset=-1, phase=-1, pan=-1, \
         client=-1, retries=1, volume=-1, filter_freq = -1, resonance = -1, bp0="", bp1="", bp2="", bp0_target=-1, bp1_target=-1, bp2_target=-1, mod_target=-1, \
-            debug=-1, mod_source=-1, eq_l = -1, eq_m = -1, eq_h = -1, filter_type= -1, algorithm=-1, ratio = -1, latency_ms = -1, algo_source=None, chorus_level=-1, chorus_delay=-1):
+        debug=-1, mod_source=-1, eq_l = -1, eq_m = -1, eq_h = -1, filter_type= -1, algorithm=-1, ratio = -1, latency_ms = -1, algo_source=None, chorus_level=-1, chorus_delay=-1):
 
     m = ""
     if(timestamp is None): timestamp = millis()
