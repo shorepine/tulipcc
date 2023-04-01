1cbac121a28aa3755a87b599ceeabc327ab917bf
Brian Whitman <brian@variogr.am>
2023-04-01 19:08:15 -0400

    updating buttons example

--- tulip_home/ex/buttons.py
+++ tulip_home/ex/buttons.py
@@ -3,13 +3,17 @@ import tulip
 def cb(x):
     if(x<2):
         print("button %d pressed" % (x))
-    else:
-        print("slider val is %f" % (tulip.slider(2)))
+    if(x==2):
+        print("slider val is %f" % (tulip.slider(x)))
+    if(x>2):
+        print("text box %d now says %s" % (x, tulip.text(x)))
 
 tulip.bg_clear()
 tulip.ui_callback(cb)
-tulip.button(0,"hey there",500,200,150,60,10,255,200,0)
+tulip.button(0,"hey there",600,200,150,60,10,255,200,0)
 tulip.button(1,"here again", 500, 400, 150, 60, 10, 255, 200, 0)
 tulip.slider(2,200,400,30,150,0,0.5,tulip.color(255,0,0),tulip.color(0,255,0))
+tulip.text(3,"12", 200,200,100,40,255,0)
+tulip.text(4,"Hello there", 400,200,100,40,255,0)
 print("done")
 
