0b465b13829443c8f07505e19885cd338e94de03
Brian Whitman <brian@variogr.am>
2023-04-02 15:12:05 -0400

    show font numbers

--- tulip_home/ex/fonts.py
+++ tulip_home/ex/fonts.py
@@ -2,5 +2,5 @@ import tulip
 tulip.gpu_reset()
 text = "The quick brown fox loves to use his lazy Tulip"
 for i in range(19):
-    tulip.str(text, 20, 20+(i*30), 255, i)
+    tulip.str("#%d: %s" % (i,text), 20, 20+(i*30), 255, i)
 
