d11733a24c5e620b7ae2684abf0af96d2f682f6e
Brian Whitman <brian@variogr.am>
2023-06-27 11:43:44 -0700

    CC -> Desktop

--- tulip/macos/build.sh
+++ tulip/macos/build.sh
@@ -7,11 +7,11 @@ source ../shared/grab_submodules.sh
 # Make debug and set up app bundle on mac
 make DEBUG=1
 mkdir -p dev
-rm -rf dev/Tulip\ CC.app
-mkdir -p dev/Tulip\ CC.app/Contents/{MacOS,Resources,Frameworks,libs}
-cp build-standard/tulip/obj/tulip.$(arch) dev/Tulip\ CC.app/Contents/MacOS/tulip
-cp Info.plist dev/Tulip\ CC.app/Contents/
-cp tulip.icns dev/Tulip\ CC.app/Contents/Resources/
-cp -rf ../fs dev/Tulip\ CC.app/Contents/Resources/tulip_home
-cp -a SDL2.framework dev/Tulip\ CC.app/Contents/Frameworks/
-install_name_tool -add_rpath @executable_path/../Frameworks dev/Tulip\ CC.app/Contents/MacOS/tulip
+rm -rf dev/Tulip\ Desktop.app
+mkdir -p dev/Tulip\ Desktop.app/Contents/{MacOS,Resources,Frameworks,libs}
+cp build-standard/tulip/obj/tulip.$(arch) dev/Tulip\ Desktop.app/Contents/MacOS/tulip
+cp Info.plist dev/Tulip\ Desktop.app/Contents/
+cp tulip.icns dev/Tulip\ Desktop.app/Contents/Resources/
+cp -rf ../fs dev/Tulip\ Desktop.app/Contents/Resources/tulip_home
+cp -a SDL2.framework dev/Tulip\ Desktop.app/Contents/Frameworks/
+install_name_tool -add_rpath @executable_path/../Frameworks dev/Tulip\ Desktop.app/Contents/MacOS/tulip
--- tulip/macos/package.sh
+++ tulip/macos/package.sh
@@ -21,16 +21,16 @@ rm tulip.arm64
 mkdir -p dist
 
 # Now make the app bundle
-rm -rf dist/Tulip\ CC.app
-mkdir -p dist/Tulip\ CC.app/Contents/{MacOS,Resources,Frameworks,libs}
-cp tulip dist/Tulip\ CC.app/Contents/MacOS
-cp Info.plist dist/Tulip\ CC.app/Contents/
-cp -rf ../fs dist/Tulip\ CC.app/Contents/Resources/
-cp -a SDL2.framework dist/Tulip\ CC.app/Contents/Frameworks/
-install_name_tool -add_rpath @executable_path/../Frameworks dist/Tulip\ CC.app/Contents/MacOS/tulip
-cp tulip.icns dist/Tulip\ CC.app/Contents/Resources/
-codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ CC.app/Contents/Frameworks/SDL2.framework/Versions/A/SDL2 -f
-codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ CC.app/Contents/MacOS/tulip -f
+rm -rf dist/Tulip\ Desktop.app
+mkdir -p dist/Tulip\ Desktop.app/Contents/{MacOS,Resources,Frameworks,libs}
+cp tulip dist/Tulip\ Desktop.app/Contents/MacOS
+cp Info.plist dist/Tulip\ Desktop.app/Contents/
+cp -rf ../fs dist/Tulip\ Desktop.app/Contents/Resources/
+cp -a SDL2.framework dist/Tulip\ Desktop.app/Contents/Frameworks/
+install_name_tool -add_rpath @executable_path/../Frameworks dist/Tulip\ Desktop.app/Contents/MacOS/tulip
+cp tulip.icns dist/Tulip\ Desktop.app/Contents/Resources/
+codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ Desktop.app/Contents/Frameworks/SDL2.framework/Versions/A/SDL2 -f
+codesign -s "Developer ID Application: Brian Whitman (Y6CQ3JU8G4)" dist/Tulip\ Desktop.app/Contents/MacOS/tulip -f
 
 # I then run "AppWrapper 4.0" to notarize the package. I would love to convert that into a command line tool, i believe xcrun notarytool
 # TODO: https://github.com/mitchellh/gon
