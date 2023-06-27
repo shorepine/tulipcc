e0a04ba978ddbd016e4eda63c2d5f1b05f91bda0
Brian Whitman <brian@variogr.am>
2023-06-27 10:11:53 -0700

    bad fix for #101

--- tulip/macos/build.sh
+++ tulip/macos/build.sh
@@ -14,6 +14,8 @@ cp Info.plist dev/Tulip\ CC.app/Contents/
 cp -rf ../fs dev/Tulip\ CC.app/Contents/Resources/tulip_home
 cp -a SDL2.framework dev/Tulip\ CC.app/Contents/Frameworks/
 install_name_tool -add_rpath @executable_path/../Frameworks dev/Tulip\ CC.app/Contents/MacOS/tulip
-
+# See https://github.com/bwhitman/tulipcc/issues/101
+rm -rf ../amy
+rm -rf ../micropython
 
 
