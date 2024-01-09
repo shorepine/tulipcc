7d3dd0275e1e5459b8e50d1e076e59bcfe568093
Brian Whitman <brian@variogr.am>
2024-01-09 13:47:14 -0800

    r10

---
+++ tulip/tulipcc_r10/boards/TULIP4_N32R8/mpconfigboard.cmake
@@ -0,0 +1,10 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    ../../micropython/ports/esp32/boards/sdkconfig.base
+    ../../micropython/ports/esp32/boards/sdkconfig.usb
+    ../../micropython/ports/esp32/boards/sdkconfig.ble
+    ../../micropython/ports/esp32/boards/sdkconfig.240mhz
+    boards/sdkconfig.tulip
+    boards/TULIP4_N32R8/sdkconfig.board
+)
