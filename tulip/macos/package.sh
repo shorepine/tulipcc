9e103b463be67702f0bea43e62a2efd999a47311
Brian Whitman <brian@variogr.am>
2024-01-24 17:04:30 -0800

    release process includes Desktop now

--- tulip/macos/package.sh
+++ tulip/macos/package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e 
 # package.sh
 # packages the Tulip Deskop binary into a macOS app, universal binary
 
@@ -7,6 +7,11 @@
 # https://medium.com/mkdir-awesome/how-to-install-x86-64-homebrew-packages-on-apple-m1-macbook-54ba295230f
 # arch -x86_64 /usr/local/homebrew/bin/brew install libffi 
 
+# I also first stored my Apple credentials with this
+# xcrun notarytool store-credentials --apple-id "brian@variogram.com" --team-id "Y6CQ3JU8G4"
+
+
+DEV_ID="Developer ID Application: Brian Whitman (Y6CQ3JU8G4)"
 # Build for arm then intel
 rm -rf build-standard
 make WHICH_ARCH=arm64
@@ -20,11 +25,10 @@ rm -rf build-standard
 lipo -create -output tulip tulip.x86_64 tulip.arm64
 rm tulip.x86_64
 rm tulip.arm64
+rm -rf dist
 mkdir -p dist
 
 # Now make the app bundle
-rm -rf dist/Tulip\ Desktop.app
-rm -rf dist/Tulip\ Desktop.zip
 mkdir -p dist/Tulip\ Desktop.app/Contents/{MacOS,Resources,Frameworks,libs}
 cp tulip dist/Tulip\ Desktop.app/Contents/MacOS
 cp Info.plist dist/Tulip\ Desktop.app/Contents/
@@ -34,4 +38,16 @@ install_name_tool -add_rpath @executable_path/../Frameworks dist/Tulip\ Desktop.
 cp tulip.icns dist/Tulip\ Desktop.app/Contents/Resources/
 # I then run SD Notary2 on this .app file. It creates a .dmg file
 
+# Yikes, but https://stackoverflow.com/questions/60608887/what-is-the-most-efficient-way-to-notarize-and-staple-a-zip-containing-a-app
+codesign -s "${DEV_ID}" dist/Tulip\ Desktop.app/Contents/Frameworks/SDL2.framework/Versions/A/SDL2 -f
+codesign -s "${DEV_ID}" dist/Tulip\ Desktop.app/Contents/MacOS/tulip -f
+codesign --force --options runtime --timestamp --sign "${DEV_ID}" dist/Tulip\ Desktop.app
+cd dist
+/usr/bin/ditto -c -k --sequesterRsrc --keepParent Tulip\ Desktop.app Tulip_Desktop.zip
+xcrun notarytool submit Tulip_Desktop.zip --keychain-profile "baw" --wait
+rm -rf Tulip\ Desktop.app
+unzip Tulip_Desktop.zip
+xcrun stapler staple Tulip\ Desktop.app
+/usr/bin/ditto -c -k --sequesterRsrc --keepParent Tulip\ Desktop.app Tulip_Desktop.zip
+cd ..
 
--- tulip/release.sh
+++ tulip/release.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # This script cleans, builds and uploads binaries for the 5 supported Tulip CC versions
+# It also does this for Tulip Desktop macOS.
+
 # call it like ./release.sh v-jan-2024 , where v-jan-2024 is a github release tag 
 # manually make a new release tag if you haven't done one in a while, or use this to overwrite the binaries on an existing tag
 # this also uploads to Tulip World for the tulip.upgrade() feature
+set -e
+
 
 die() { echo "$*" 1>&2 ; exit 1; }
 
@@ -10,7 +14,7 @@ die() { echo "$*" 1>&2 ; exit 1; }
 
 RELEASE=$1
 
-set -e
+cd esp32s3
 declare -a boards=("TULIP4_R10" "MATOUCH7" "N16R8" "N32R8" "TDECK")
 for i in "${boards[@]}"
 do
@@ -22,6 +26,15 @@ do
 done
 python upload_firmware.py sys
 gh release upload --clobber $RELEASE dist/tulip-sys.bin
+
+
+# Now do desktop
+cd ../macos
+./package.sh
+gh release upload --clobber $RELEASE dist/Tulip_Desktop.zip 
+cd ..
+
+
 
 
 
