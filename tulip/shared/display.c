618bd0b1e2224b78295739a7bfe649977c6c2057
Brian Whitman <brian@variogr.am>
2024-01-26 07:31:20 -0800

    much better utf8 -> cp437 for #158

--- tulip/shared/display.c
+++ tulip/shared/display.c
@@ -735,29 +735,7 @@ uint8_t ansi_parse_digits(unsigned char*str, uint16_t j, uint16_t k, uint16_t * 
     return d;
 }
 
-// Micropython will send utf-8 codes through the REPL, here we convert them back to cp437 codes
-uint8_t unicode_byte_to_cp437(uint8_t x) {
-    static const uint16_t uc437[] = {  /*  CP437(U.S.) to Unicode conversion table */
-       0x00C7, 0x00FC, 0x00E9, 0x00E2, 0x00E4, 0x00E0, 0x00E5, 0x00E7, 0x00EA, 0x00EB, 0x00E8, 0x00EF, 0x00EE, 0x00EC, 0x00C4, 0x00C5,
-       0x00C9, 0x00E6, 0x00C6, 0x00F4, 0x00F6, 0x00F2, 0x00FB, 0x00F9, 0x00FF, 0x00D6, 0x00DC, 0x00A2, 0x00A3, 0x00A5, 0x20A7, 0x0192,
-       0x00E1, 0x00ED, 0x00F3, 0x00FA, 0x00F1, 0x00D1, 0x00AA, 0x00BA, 0x00BF, 0x2310, 0x00AC, 0x00BD, 0x00BC, 0x00A1, 0x00AB, 0x00BB,
-       0x2591, 0x2592, 0x2593, 0x2502, 0x2524, 0x2561, 0x2562, 0x2556, 0x2555, 0x2563, 0x2551, 0x2557, 0x255D, 0x255C, 0x255B, 0x2510,
-       0x2514, 0x2534, 0x252C, 0x251C, 0x2500, 0x253C, 0x255E, 0x255F, 0x255A, 0x2554, 0x2569, 0x2566, 0x2560, 0x2550, 0x256C, 0x2567,
-       0x2568, 0x2564, 0x2565, 0x2559, 0x2558, 0x2552, 0x2553, 0x256B, 0x256A, 0x2518, 0x250C, 0x2588, 0x2584, 0x258C, 0x2590, 0x2580,
-       0x03B1, 0x00DF, 0x0393, 0x03C0, 0x03A3, 0x03C3, 0x00B5, 0x03C4, 0x03A6, 0x0398, 0x03A9, 0x03B4, 0x221E, 0x03C6, 0x03B5, 0x2229,
-       0x2261, 0x00B1, 0x2265, 0x2264, 0x2320, 0x2321, 0x00F7, 0x2248, 0x00B0, 0x2219, 0x00B7, 0x221A, 0x207F, 0x00B2, 0x25A0, 0x00A0
-    };
-    uint16_t find = 0x0000 | x;
-    for(uint8_t i=0;i<128;i++) {
-        if(uc437[i]==find) {
-            fprintf(stderr, "found %d from %d\n", i, x);
-            return i+128;
-        }
-    }
-    fprintf(stderr, "coulnd't find %d\n", x);
-    return 32; //empty
-}
-
+uint32_t utf8_esc = 0;
 uint8_t supress_lf = 0;
 void display_tfb_str(unsigned char*str, uint16_t len, uint8_t format, uint8_t fg_color, uint8_t bg_color) {
     if(tfb_log) {
@@ -773,13 +751,15 @@ void display_tfb_str(unsigned char*str, uint16_t len, uint8_t format, uint8_t fg
         if(str[i] == 8)  { // backspace , go backwards (don't delete)
             display_tfb_uncursor(tfb_x_col, tfb_y_row);
             if(tfb_x_col > 0) tfb_x_col--;
-        } else if(str[i] == 0xc2) {
-            // eat it, mangle the next one to cp437
-            str[i+1] = unicode_byte_to_cp437(str[i+1]);
-            i++;
-        } else if(str[i] == 0xc3) {
-            str[i+1] = unicode_byte_to_cp437(str[i+1]+64);
-            i++;
+        }
+        if(str[i]>127) { // unicode
+            uint32_t esc = 0;
+            uint8_t code= convert_utf8_to_cp437(str[i], &esc);
+            while(code == 0) {
+                i++;
+                code = convert_utf8_to_cp437(str[i], &esc);
+            }
+            str[i] = code;
         }
         if(str[i] == 27) { // ANSI
             // we see an esc coming in on stream at i
--- tulip/shared/keyscan.c
+++ tulip/shared/keyscan.c
@@ -12,6 +12,178 @@ extern int mp_interrupt_char;
 uint8_t last_scan[8] = {0,0,0,0,0,0,0,0};
 
 
+
+// I pulled this from a Linux commit
+/*
+ * Convert UTF-8 bytes into a code page 437 character.
+ * Based on the table in the Code_page_437 Wikipedia page.
+*/
+
+
+uint8_t code_points_00a0[] = {
+   255, 173, 155, 156,   7, 157,   7,  21,
+     7,   7, 166, 174, 170,   7,   7,   7,
+   248, 241, 253,   7,   7, 230,  20, 250,
+     7,   7, 167, 175, 172, 171,   7, 168,
+     7,   7,   7,   7, 142, 143, 146, 128,
+     7, 144,   7,   7,   7,   7,   7,   7,
+     7, 165,   7,   7,   7,   7, 153,   7,
+     7,   7,   7,   7, 154,   7,   7, 225,
+   133, 160, 131,   7, 132, 134, 145, 135,
+   138, 130, 136, 137, 141, 161, 140, 139,
+     7, 164, 149, 162, 147,   7, 148, 246,
+     7, 151, 163, 150, 129,   7,   7, 152,
+};
+
+uint8_t code_points_2550[] = {
+   205, 186, 213, 214, 201, 184, 183, 187,
+   212, 211, 200, 190, 189, 188, 198, 199,
+   204, 181, 182, 185, 209, 210, 203, 207,
+   208, 202, 216, 215, 206
+};
+
+static uint8_t utf8_convert_11bit(uint16_t code)
+{
+   switch (code) {
+   case 0x0192: return 159;
+   case 0x0393: return 226;
+   case 0x0398: return 233;
+   case 0x03A3: return 228;
+   case 0x03A6: return 232;
+   case 0x03A9: return 234;
+   case 0x03B1: return 224;
+   case 0x03B4: return 235;
+   case 0x03B5: return 238;
+   case 0x03C0: return 227;
+   case 0x03C3: return 229;
+   case 0x03C4: return 231;
+   case 0x03C6: return 237;
+   }
+
+   return 0;
+};
+
+static uint8_t utf8_convert_2xxx(uint16_t code)
+{
+   switch (code) {
+   case 0x2022: return 7;
+   case 0x203C: return 19;
+   case 0x207F: return 252;
+   case 0x20A7: return 158;
+   case 0x2190: return 27;
+   case 0x2191: return 24;
+   case 0x2192: return 26;
+   case 0x2193: return 25;
+   case 0x2194: return 29;
+   case 0x2195: return 18;
+   case 0x21A8: return 23;
+   case 0x2219: return 249;
+   case 0x221A: return 251;
+   case 0x221E: return 236;
+   case 0x221F: return 28;
+   case 0x2229: return 239;
+   case 0x2248: return 247;
+   case 0x2261: return 240;
+   case 0x2264: return 243;
+   case 0x2265: return 242;
+   case 0x2310: return 169;
+   case 0x2320: return 244;
+   case 0x2321: return 245;
+   case 0x2500: return 196;
+   case 0x2502: return 179;
+   case 0x250C: return 218;
+   case 0x2510: return 191;
+   case 0x2514: return 192;
+   case 0x2518: return 217;
+   case 0x251C: return 195;
+   case 0x2524: return 180;
+   case 0x252C: return 194;
+   case 0x2534: return 193;
+   case 0x253C: return 197;
+   case 0x2580: return 223;
+   case 0x2584: return 220;
+   case 0x2588: return 219;
+   case 0x258C: return 221;
+   case 0x2590: return 222;
+   case 0x2591: return 176;
+   case 0x2592: return 177;
+   case 0x2593: return 178;
+   case 0x25A0: return 254;
+   case 0x25AC: return 22;
+   case 0x25B2: return 30;
+   case 0x25BA: return 16;
+   case 0x25BC: return 31;
+   case 0x25C4: return 17;
+   case 0x25CB: return 9;
+   case 0x25D8: return 8;
+   case 0x25D9: return 10;
+   case 0x263A: return 1;
+   case 0x263B: return 2;
+   case 0x263C: return 15;
+   case 0x2640: return 12;
+   case 0x2642: return 11;
+   case 0x2660: return 6;
+   case 0x2663: return 5;
+   case 0x2665: return 3;
+   case 0x2666: return 4;
+   case 0x266A: return 13;
+   case 0x266B: return 14;
+   }
+
+   return 0;
+}
+
+uint8_t convert_uc16_to_cp437(uint16_t code)
+{
+   if (code < 0x7f)        // ASCII
+       return code;
+   if (code < 0xa0)        // high control characters
+       return code;
+   if (code < 0x100)       // international characters
+       return code_points_00a0[code - 0xa0];
+   if (code < 0x800)
+       return utf8_convert_11bit(code);
+   if (code >= 0x2550 && code < 0x256d)    // block graphics
+       return code_points_2550[code - 0x2550];
+
+   return utf8_convert_2xxx(code);
+}
+
+uint8_t convert_utf8_to_cp437(uint8_t c, uint32_t *esc)
+{
+   int shift;
+   uint16_t ucs;
+
+   if (c < 127)            // ASCII
+       return c;
+   if (c == 127)
+       return 8;       // DEL (?)
+
+   switch (c & 0xf0) {
+   case 0xc0: case 0xd0:       // two bytes sequence
+       *esc = (1U << 24) | ((c & 0x1f) << 6);
+       return 0;
+   case 0xe0:          // three bytes sequence
+       *esc = (2U << 24) | ((c & 0x0f) << 12);
+       return 0;
+   case 0xf0:          // four bytes sequence
+       *esc = (3U << 24) | ((c & 0x07) << 18);
+       return 0;
+   case 0x80: case 0x90: case 0xa0: case 0xb0: // continuation
+       shift = (*esc >> 24) - 1;
+       ucs = *esc & 0xffffff;
+       if (shift) {
+           *esc = (shift << 24) | ucs | (c & 0x3f) << (shift * 6);
+           return 0;
+       }
+       *esc = 0;
+       return convert_uc16_to_cp437(ucs | (c & 0x3f));
+   }
+
+   return 0;
+}
+
+
 uint16_t scan_ascii(uint8_t code, uint32_t modifier) {
 #ifndef ESP_PLATFORM
     uint8_t shift = (modifier & KMOD_LSHIFT || modifier & KMOD_RSHIFT);
