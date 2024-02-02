e8a03a6e846959adf8209ed011bc75479316414b
Brian Whitman <brian@variogr.am>
2024-02-02 12:00:09 -0800

    dac and adc exmaple

---
+++ tulip/fs/ex/m5adc.py
@@ -0,0 +1,15 @@
+# driver for m5stack dac2
+from machine import Pin, I2C
+import time
+
+i2c = I2C(0, scl=Pin(18), sda=Pin(17), freq=400000)
+
+def get():
+    b = i2c.readfrom(0x48,3)
+    val = (b[0] << 8) | (b[1] & 0xff)
+    # 26435 = 10V measured from the dac2
+    return (val / 26435.0)*10.0
+
+
+
+
