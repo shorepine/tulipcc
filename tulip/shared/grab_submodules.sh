5c5c4d6c9462df151649ad696fc4d40eeed05f29
Brian Whitman <brian@variogr.am>
2024-02-12 08:04:34 -0800

    moving lv_binding to place we can control

--- tulip/shared/grab_submodules.sh
+++ tulip/shared/grab_submodules.sh
@@ -11,13 +11,13 @@ else
     echo "Syncing submodules for first build..."
     git submodule update --init amy
     git submodule update --init micropython
+    git submodule update --init --recursive lv_binding_micropython
     cd micropython
     git submodule update --init lib/axtls 
     git submodule update --init lib/libffi 
     git submodule update --init lib/mbedtls
     git submodule update --init lib/berkeley-db-1.xx
     git submodule update --init lib/micropython-lib 
-    git submodule update --init --recursive lib/lv_binding_micropython
     cd ..
     touch .submodules_ok
 fi
