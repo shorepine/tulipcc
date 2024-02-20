f1af25ba522db1f77a156f556e8349e2268894a7
Brian Whitman <brian@variogr.am>
2024-02-19 16:36:14 -0800

    custom.c in tulipCC

--- tulip/esp32s3/esp32_common.cmake
+++ tulip/esp32s3/esp32_common.cmake
@@ -129,6 +129,7 @@ list(APPEND MICROPY_SOURCE_EXTMOD
     ${AMY_DIR}/src/dsps_biquad_f32_ae32.S
     ${AMY_DIR}/src/algorithms.c
     ${AMY_DIR}/src/patches.c
+    ${AMY_DIR}/src/custom.c
     ${AMY_DIR}/src/amy.c
     ${AMY_DIR}/src/delay.c
     ${AMY_DIR}/src/envelope.c
