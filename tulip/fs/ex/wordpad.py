89cf3e0128ae16a33cfca2f637eefb1b679b0eb3
Brian Whitman <brian@variogr.am>
2024-02-22 16:12:53 -0800

    wordpad update

--- tulip/fs/ex/wordpad.py
+++ tulip/fs/ex/wordpad.py
@@ -1,8 +1,7 @@
 # wordpad.py
 # use lvgl to make a full screen editor with proportional fonts and word wrap
-
 import lvgl as lv
-import tulip
+import tulip, time
 (H_RES,V_RES) = tulip.screen_size()
 
 tulip.tfb_stop()
@@ -11,10 +10,8 @@ try:
     wordpad = lv.obj()
     wordpad.set_style_bg_color(tulip.pal_to_lv(255), lv.PART.MAIN)
     ta = lv.textarea(wordpad)
-    ta.set_x(0)
-    ta.set_y(0)
-    ta.set_width(H_RES)
-    ta.set_height(V_RES)
+    ta.set_pos(0,0)
+    ta.set_size(H_RES, V_RES)
     ta.set_style_text_font(lv.font_montserrat_24, 0)
     ta.set_style_bg_color(tulip.pal_to_lv(255), lv.PART.MAIN)
     ta.set_style_text_color(tulip.pal_to_lv(0),0)
@@ -25,7 +22,7 @@ try:
     lv.group_focus_obj(ta)
 
     while True:
-        pass
+        time.sleep(1)
 
 except KeyboardInterrupt:
     lv.screen_load(tulip.lv_scr)
