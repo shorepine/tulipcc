ecd785f4e89a62631591f86e698192f52cbea519
Brian Whitman <brian@variogr.am>
2024-03-25 13:07:54 -0400

    ensure JTAG can type into Tdeck

--- tulip/esp32s3/main.c
+++ tulip/esp32s3/main.c
@@ -217,7 +217,9 @@ void mp_task(void *pvParameter) {
     #if CONFIG_USB_ENABLED
     //usb_init();
     #elif CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG
-    //usb_serial_jtag_init();
+    #ifdef TDECK
+    usb_serial_jtag_init();
+    #endif
     #endif
     #if MICROPY_HW_ENABLE_UART_REPL
     uart_stdout_init();
