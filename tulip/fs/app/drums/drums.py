2ebb1df1b516fb1813413f986636fc7e3f4fc88e
Brian Whitman <brian@variogr.am>
2024-03-30 19:10:17 -0400

    adding drum machine

---
+++ tulip/fs/app/drums/drums.py
@@ -0,0 +1,251 @@
+# drums.py
+# lvgl drum machine for Tulip
+
+from tulip import UIScreen, UIElement, pal_to_lv, lv_depad, lv, frame_callback, ticks_ms, seq_add_callback, seq_remove_callback, seq_ppq, ticks_ms
+import amy
+
+# A single drum machine switch with LED
+class DrumSwitch(UIElement):
+    led_off_color = 255
+    led_on_color = 224
+    # R O Y W 808 colors
+    switch_colors = [128,208,248,255]
+    button_width = 30
+    button_height = 40
+
+    def __init__(self, switch_color_idx=0):
+        super().__init__() 
+
+        self.group.set_size(DrumSwitch.button_width+10,DrumSwitch.button_height+25)
+        lv_depad(self.group)
+
+        top_rect = lv.obj(self.group)
+        top_rect.set_style_bg_color(pal_to_lv(DrumSwitch.switch_colors[switch_color_idx]), 0)
+        top_rect.set_size(DrumSwitch.button_width, 20)
+        lv_depad(top_rect)
+
+        top_rect.align_to(self.group, lv.ALIGN.TOP_MID, 0, 0)
+        bottom_rect = lv.obj(self.group)
+        bottom_rect.set_size(DrumSwitch.button_width, DrumSwitch.button_height)
+        bottom_rect.set_style_radius(0,0)
+        bottom_rect.align_to(top_rect, lv.ALIGN.OUT_BOTTOM_MID,0,0)
+        bottom_rect.set_style_bg_color(pal_to_lv(DrumSwitch.switch_colors[switch_color_idx]), 0)
+        bottom_rect.add_event_cb(self.cb, lv.EVENT.CLICKED, None)
+        lv_depad(bottom_rect)
+
+        self.led = lv.led(self.group)
+        self.led.set_size(5,5)
+        self.led.align_to(top_rect, lv.ALIGN.CENTER,0,0)
+        self.led.set_brightness(150)
+        self.led.set_color(pal_to_lv(DrumSwitch.led_on_color))
+        self.led.off()
+        self.on = False
+
+
+    def set(self, val):
+        if(val):
+            self.on = True
+            self.led.on()
+        else:
+            self.off = False
+            self.led.off()
+
+    def get(self):
+        return self.on
+
+    def cb(self, e):
+        if(self.on):
+            self.on = False
+            self.led.off()
+        else:
+            self.on = True
+            self.led.on()
+
+# A row of LEDs to keep time with and some labels
+class LEDStrip(UIElement): 
+    def __init__(self):
+        super().__init__() 
+        self.leds= []
+        self.group.set_size((DrumSwitch.button_width+12)*16+175+100,DrumSwitch.button_height+65)
+        for i in range(16):
+            l = lv.led(self.group)
+            l.set_size(10,10)
+            l.set_brightness(150)
+            if(i%4==0):
+                l.set_color(pal_to_lv(DrumSwitch.led_on_color))
+            else:
+                l.set_color(pal_to_lv(92))                
+            l.off()
+            if i==0:
+                l.align_to(self.group, lv.ALIGN.LEFT_MID,105,0)
+            else:
+                l.align_to(self.leds[i-1], lv.ALIGN.OUT_RIGHT_MID, DrumSwitch.button_width, 0)
+            self.leds.append(l)
+        descs = lv.label(self.group)
+        descs.set_text("Vol             Pitch            Pan")
+        descs.align_to(self.leds[-1], lv.ALIGN.OUT_RIGHT_MID, 45, 0)
+        self.group.remove_flag(lv.obj.FLAG.SCROLLABLE)
+
+    def set(self, idx, val):
+        if(val):
+            self.leds[idx].on()
+        else:
+            self.leds[idx].off()
+
+
+
+# A row of drum switches with a dropdown on the left for tone
+# and maybe volume or other knob on right idk 
+class DrumRow(UIElement):
+    knob_color = 209
+    def __init__(self, items):
+        super().__init__() 
+        self.objs = []
+        # Droodown on left
+        self.group.set_size((DrumSwitch.button_width+12)*16+175+100,DrumSwitch.button_height+65)
+        self.dropdown = lv.dropdown(self.group)
+        self.dropdown.set_options("\n".join(items))
+        self.dropdown.set_dir(lv.DIR.BOTTOM)
+        self.dropdown.add_event_cb(self.cb, lv.EVENT.VALUE_CHANGED, None)
+        self.dropdown.set_height(40)
+        self.dropdown.set_width(100)
+        self.dropdown.align_to(self.group, lv.ALIGN.LEFT_MID,0,0)
+        lv_depad(self.dropdown)
+        for i in range(4):
+            for j in range(4):
+                d = DrumSwitch(switch_color_idx=i)
+                self.objs.append(d)
+                d.group.set_parent(self.group)
+                d.group.set_style_bg_color(pal_to_lv(UIScreen.default_bg_color),lv.PART.MAIN)
+                dc = len(self.objs)-1
+                if(dc==0):
+                    d.group.align_to(self.dropdown, lv.ALIGN.OUT_RIGHT_MID,10,0)
+                else:
+                    d.group.align_to(self.objs[dc-1].group, lv.ALIGN.OUT_RIGHT_MID,0,0)
+                d.group.remove_flag(lv.obj.FLAG.SCROLLABLE)
+        # volume / vel knob
+        self.knobs = []
+        for i in range(3):
+            k = lv.arc(self.group)
+            k.set_size(50,50)
+            k.set_style_pad_hor(0,lv.PART.KNOB)
+            k.set_style_pad_ver(0,lv.PART.KNOB)
+            if(i==2): # pan is special color white/red
+                k.set_style_bg_color(pal_to_lv(255), lv.PART.KNOB)
+                k.set_style_arc_color(pal_to_lv(224), lv.PART.INDICATOR)
+            else: # one color
+                k.set_style_bg_color(pal_to_lv(DrumRow.knob_color), lv.PART.KNOB)
+                k.set_style_arc_color(pal_to_lv(DrumRow.knob_color), lv.PART.INDICATOR)
+            if(i==0):
+                k.align_to(self.objs[-1].group, lv.ALIGN.OUT_RIGHT_MID,15,0)
+            else:
+                k.align_to(self.knobs[i-1], lv.ALIGN.OUT_RIGHT_MID,15,0)
+            self.knobs.append(k)
+
+
+        lv_depad(self.group)
+        self.group.remove_flag(lv.obj.FLAG.SCROLLABLE)
+
+    def get(self, idx):
+        return self.objs[idx].get()
+
+    def set_vel(self, val):
+        self.knobs[0].set_value(int(val*100.0))
+
+    def get_vel(self):
+        return self.knobs[0].get_value()/100.0
+
+    def set_pitch(self, val):
+        self.knobs[1].set_value(int(val*100.0))
+
+    def get_pitch(self):
+        return self.knobs[1].get_value()/100.0
+
+    def set_pan(self, val):
+        self.knobs[2].set_value(int(val*100.0))
+
+    def get_pan(self):
+        return self.knobs[2].get_value()/100.0
+
+    def get_preset(self):
+        return self.dropdown.get_selected()
+
+    def set_preset(self, idx):
+        self.dropdown.set_selected(idx)
+
+    def cb(self, e):
+        pass
+
+drumkit = [ 
+    (89, "Maraca"),
+    (39, "Kick"),
+    (45, "Snare"),
+    (52, "Snare2"),
+    (51, "Snare3"),
+    (41, "Snare4"),
+    (53, "Closed Hat"),
+    (56, "Open Hat"),
+    (61, "Low Tom"),
+    (94, "Clap"),
+    (69, "Cowbell"),
+    (74, "Congo Low"),
+    (82, "Clave"),
+    (60, "Block"),
+    (60, "Roll"),
+    (60, "Hit"),
+    (60, "Crash"),
+    (60, "Shell"),
+    (60, "Chimes"),
+    (60, "Seashore"),
+    (60, "Pwr Snare"),
+    (60, "Hi Tom"),
+    (66, "Shamisen"),
+    (66, "Koto"),
+    (72, "Steel"),
+    (60, "Pwr Kick"),
+    (66, "Marimba"),
+    (60, "Frets"),
+    (60, "Std Kick")
+    ]
+
+# Called from tulip's sequencer
+def beat_callback(t):
+    global app
+    # Hm, how should we deal with visual latency 
+    app.leds.set((app.current_beat-1)% 16, 0)
+    app.leds.set(app.current_beat, 1)
+
+    # todo there's something broken about pcm patches in amy , set patch on an osc then change it, its out of pitch
+    for i, row in enumerate(app.rows):
+        if(row.get(app.current_beat)):
+            base_note = drumkit[row.get_preset()][0]
+            note_for_pitch = int(base_note + (row.get_pitch() - 0.5)*24.0)
+            amy.send(osc=50+i+2, wave=amy.PCM, patch=row.get_preset(), note=note_for_pitch, vel=row.get_vel()*2, pan=row.get_pan(), time=t)
+
+    app.current_beat = (app.current_beat+1) % 16
+
+def quit(screen):
+    seq_remove_callback(beat_callback)
+
+def run(screen):
+    global app 
+    app = screen # we can use the screen obj passed in as a general "store stuff here" class, as well as inspect the UI 
+    app.current_beat = 0
+    app.offset_y = 10
+    app.set_bg_color(0)
+    app.quit_callback = quit
+    app.leds = LEDStrip()
+    app.add(app.leds, direction=lv.ALIGN.OUT_BOTTOM_LEFT, pad_y=0)
+    app.rows = [DrumRow([x[1] for x in drumkit]) for i in range(7)]
+    app.add(app.rows, direction=lv.ALIGN.OUT_BOTTOM_LEFT)
+
+    for i,row in enumerate(app.rows):
+        row.set_preset([1, 2, 0, 6, 7, 9, 10][i])
+        row.set_vel(.5)
+        row.set_pan(.5)
+        row.set_pitch(.5)
+
+    seq_add_callback(beat_callback, int(seq_ppq()/2))
+    app.present()
+
+
