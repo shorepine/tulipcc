4f3160d7cb1b8f2163e653636808397f003ad715
Brian Whitman <brian@variogr.am>
2024-03-31 07:46:04 -0400

    fix tabs

--- tulip/fs/app/juno6/polyvoice.py
+++ tulip/fs/app/juno6/polyvoice.py
@@ -2,88 +2,85 @@
 
 # Optional monkeypatch of send() method to diagnose exactly what is being sent.
 def amy_send_patch(**kwargs):
-        print("amy_send:", kwargs)
-        orig_amy_send(**kwargs)
+    print("amy_send:", kwargs)
+    orig_amy_send(**kwargs)
 
-# Apply the monkeypatch?
-#orig_amy_send = amy.send
-#amy.send = amy_send_patch
 
 # Micropython collections.deque does not support remove.
 class Queue:
-        def __init__(self, maxsize=64, name=''):
-                self.name = name    # Just for debug.
-                self.maxsize = maxsize + 1
-                self.queue = [None] * self.maxsize
-                self.head = 0
-                self.tail = 0
+    def __init__(self, maxsize=64, name=''):
+            self.name = name    # Just for debug.
+            self.maxsize = maxsize + 1
+            self.queue = [None] * self.maxsize
+            self.head = 0
+            self.tail = 0
 
-        def _next(self, pointer):
-                """Incrementing a cicular buffer pointer."""
-                return (pointer + 1) % self.maxsize
+    def _next(self, pointer):
+        """Incrementing a cicular buffer pointer."""
+        return (pointer + 1) % self.maxsize
                 
-        def _prev(self, pointer):
-                """Decrementing a cicular buffer pointer."""
-                return (pointer + self.maxsize - 1) % self.maxsize
+    def _prev(self, pointer):
+        """Decrementing a cicular buffer pointer."""
+        return (pointer + self.maxsize - 1) % self.maxsize
                 
-        def put(self, item):
-                self.queue[self.tail] = item
-                self.tail = self._next(self.tail)
-                if self.tail == self.head:
-                        # Wrap around
-                        self.head = self._next(self.head)
-                        print("%s: dropped oldest item", self)
+    def put(self, item):
+        self.queue[self.tail] = item
+        self.tail = self._next(self.tail)
+        if self.tail == self.head:
+            # Wrap around
+            self.head = self._next(self.head)
+            print("%s: dropped oldest item", self)
 
-        def _delete_at(self, pointer):
-                """Remove the value at queue[pointer], and close up the rest."""
-                if self.tail > pointer:
-                        self.queue[pointer : self.tail - 1] = (
-                                self.queue[pointer + 1 : self.tail])
-                        self.tail = self._prev(self.tail)
-                elif self.tail < pointer:
-                        # Shift head: pointer one step forward.
-                        self.queue[self.head + 1 : pointer + 1] = self.queue[self.head : pointer]
-                        self.head = self._next(self.head)
-                else:
-                        raise ValueError('pointer at tail???')
+    def _delete_at(self, pointer):
+        """Remove the value at queue[pointer], and close up the rest."""
+        if self.tail > pointer:
+            self.queue[pointer : self.tail - 1] = (
+                self.queue[pointer + 1 : self.tail])
+            self.tail = self._prev(self.tail)
+        elif self.tail < pointer:
+            # Shift head: pointer one step forward.
+            self.queue[self.head + 1 : pointer + 1] = self.queue[self.head : pointer]
+            self.head = self._next(self.head)
+        else:
+            raise ValueError('pointer at tail???')
 
-        def remove(self, value):
-                """Remove first occurrence of value from queue."""
-                pointer = self.head
-                while pointer != self.tail:
-                        if self.queue[pointer] == value:
-                                self._delete_at(pointer)
-                                return
-                        pointer = self._next(pointer)
-                # Fell through, value wasn't found.
-                raise ValueError('%s: No such item: %s' % (self, str(value)))
+    def remove(self, value):
+        """Remove first occurrence of value from queue."""
+        pointer = self.head
+        while pointer != self.tail:
+            if self.queue[pointer] == value:
+                self._delete_at(pointer)
+                return
+            pointer = self._next(pointer)
+        # Fell through, value wasn't found.
+        raise ValueError('%s: No such item: %s' % (self, str(value)))
 
-        def empty(self):
-                return self.head == self.tail
+    def empty(self):
+        return self.head == self.tail
 
-        def full(self):
-                return self.head == self._next(self.tail)
+    def full(self):
+        return self.head == self._next(self.tail)
 
-        def qsize(self):
-                return (self.tail - self.head + self.maxsize) % self.maxsize
+    def qsize(self):
+        return (self.tail - self.head + self.maxsize) % self.maxsize
 
-        def get(self):
-                if self.empty():
-                        # get() on empty queue.
-                        raise ValueError
-                value = self.queue[self.head]
-                self.head = self._next(self.head)
-                return value
+    def get(self):
+        if self.empty():
+            # get() on empty queue.
+            raise ValueError
+        value = self.queue[self.head]
+        self.head = self._next(self.head)
+        return value
 
-        def __repr__(self):
-                result = []
-                p = self.head
-                while p != self.tail:
-                        result.append(self.queue[p])
-                        p = self._next(p)
-                return ("Queue%s(maxsize=%d) [" % (self.name, self.maxsize - 1)
-                                + (", ".join(str(s) for s in result))
-                                + "]")
+    def __repr__(self):
+        result = []
+        p = self.head
+        while p != self.tail:
+            result.append(self.queue[p])
+            p = self._next(p)
+        return ("Queue%s(maxsize=%d) [" % (self.name, self.maxsize - 1)
+            + (", ".join(str(s) for s in result))
+            + "]")
 
 
 class Synth:
@@ -184,12 +181,12 @@ def midi_event_cb(x):
         elif m[0] == 0xbf:
             # Special case for Oxygen49 transport buttons which send val 0x00 on release.
             if m[2] == 0x7f:
-                    control_change_fn(m[1], m[2])
+                control_change_fn(m[1], m[2])
                 
         # Are there more events waiting?
         m = m[3:]
         if len(m) == 0:
-                m = midi_in_fn()
+            m = midi_in_fn()
 
 
 
@@ -201,10 +198,6 @@ def init(synth=None, my_midi_in_fn=None, my_control_change_fn=None, my_set_patch
     midi_in_fn = my_midi_in_fn
     control_change_fn = my_control_change_fn
     set_patch_fn = my_set_patch_fn
-
-    #if not synth:
-    #        import juno
-    #        synth = juno.JunoPatch.from_patch_number(0)
     
     NUM_VOICES = 8
     SYNTH = Synth(synth, NUM_VOICES)
