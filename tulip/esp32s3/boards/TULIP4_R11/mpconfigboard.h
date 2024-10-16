ee8cff5aae555b592e249833a66c63905018f0e8
Brian Whitman <brian@variogr.am>
2024-10-16 14:45:11 -0400

    local mpconfigport that overwrites the ports/esp32 one for MP_SCHEDULER_DEPTH

--- tulip/esp32s3/boards/TULIP4_R11/mpconfigboard.h
+++ tulip/esp32s3/boards/TULIP4_R11/mpconfigboard.h
@@ -7,16 +7,3 @@
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
 
-
-#define MICROPY_HW_I2C0_SCL                 (I2C_SCL)
-#define MICROPY_HW_I2C0_SDA                 (I2C_SDA)
-
-#define MICROPY_HW_ENABLE_SDCARD            (0)
-#define MICROPY_PY_MACHINE_I2S              (0)
-#define MICROPY_PY_BLUETOOTH (0)
-#define MICROPY_BLUETOOTH_NIMBLE (0)
-#define MICROPY_HW_USB_CDC (1)
-#define MICROPY_HW_ENABLE_USBDEV (1)
-#define MICROPY_HW_ESP_USB_SERIAL_JTAG (0)
-#define MICROPY_ENABLE_SCHEDULER (1)
-
