b1d87da4bc27f04c308a36026c7797f758cec68f
Brian Whitman <brian@variogr.am>
2025-12-08 16:24:43 -0800

    amyboard usb midi out

--- tulip/shared/amy_connector.c
+++ tulip/shared/amy_connector.c
@@ -152,10 +152,11 @@ void run_amy() {
     amy_config_t amy_config = amy_default_config();
 #ifdef AMYBOARD
     amy_config.features.audio_in = 1;
+    amy_config.midi = AMY_MIDI_IS_UART | AMY_MIDI_IS_USB_GADGET
 #else
+    amy_config.midi = AMY_MIDI_IS_UART;
     amy_config.features.audio_in = 0;
 #endif
-    amy_config.midi = AMY_MIDI_IS_UART;
     amy_config.features.default_synths = 0; // midi.py does this for us
     amy_config.i2s_lrc = CONFIG_I2S_LRCLK;
     amy_config.i2s_bclk = CONFIG_I2S_BCLK;
