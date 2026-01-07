0d591a395a930a49600ce15a25c2df5d3fd6c35d
Brian Whitman <brian@variogr.am>
2026-01-06 18:21:22 -0800

    remvoing old amy deploy

--- tulip/amyboardweb/build.sh
+++ tulip/amyboardweb/build.sh
@@ -15,23 +15,23 @@ make
 # Now modify the static html and copy everything to a stage area 
 rm -rf stage
 mkdir stage
-mkdir stage/amy
 
-cp static/* stage/amy
+cp static/* stage/
 cp -Rf ../../www/img stage
-cp -Rf ../../www/webfonts stage
+cp -Rf ../../www/fonts stage
 cp -Rf ../../www/css stage
+cp -Rf ../../www/js stage
 
-cp ../../amy/docs/amy.js stage/amy/amy-$timestamp.js
-cp ../../amy/docs/amy.wasm stage/amy/amy-$timestamp.wasm
-cp ../../amy/docs/amy.aw.js stage/amy/amy-$timestamp.aw.js
+cp ../../amy/docs/amy.js stage/amy-$timestamp.js
+cp ../../amy/docs/amy.wasm stage/amy-$timestamp.wasm
+cp ../../amy/docs/amy.aw.js stage/amy-$timestamp.aw.js
 
-cp build-standard/tulip/obj/micropython.wasm stage/amy/amyboard-$timestamp.wasm
-cp build-standard/tulip/obj/micropython.mjs stage/amy/amyboard-$timestamp.mjs
-cp build-standard/tulip/obj/micropython.data stage/amy/amyboard-$timestamp.data
+cp build-standard/tulip/obj/micropython.wasm stage/amyboard-$timestamp.wasm
+cp build-standard/tulip/obj/micropython.mjs stage/amyboard-$timestamp.mjs
+cp build-standard/tulip/obj/micropython.data stage/amyboard-$timestamp.data
 
-sed -i '' -e "s/AMYBOARDMJS/amyboard\-${timestamp}.mjs/g" -e "s/AMYJS/amy\-${timestamp}.js/g" stage/amy/index.html
-sed -i '' -e "s/amy.aw.js/amy\-${timestamp}.aw.js/g" -e "s/amy.wasm/amy\-${timestamp}.wasm/g" stage/amy/amy-$timestamp.js
-sed -i '' -e "s/micropython./amyboard\-${timestamp}./g" stage/amy/amyboard-$timestamp.mjs
+sed -i '' -e "s/AMYBOARDMJS/amyboard\-${timestamp}.mjs/g" -e "s/AMYJS/amy\-${timestamp}.js/g" stage/index.html
+sed -i '' -e "s/amy.aw.js/amy\-${timestamp}.aw.js/g" -e "s/amy.wasm/amy\-${timestamp}.wasm/g" stage/amy-$timestamp.js
+sed -i '' -e "s/micropython./amyboard\-${timestamp}./g" stage/amyboard-$timestamp.mjs
 
 python3 server.py
