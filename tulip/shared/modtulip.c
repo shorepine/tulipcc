974d3f861aae49dded18ace9fee37a19fa5c3e57
Brian Whitman <brian@variogram.com>
2026-02-12 08:08:58 -0800

    Add opt-in external MIDI realtime sync toggle

--- tulip/shared/modtulip.c
+++ tulip/shared/modtulip.c
@@ -12,6 +12,7 @@
 #ifndef __EMSCRIPTEN__
 #include "amy.h"
 #endif
+#include "amy_midi.h"
 #include "amy_connector.h"
 #include "tsequencer.h"
 #if !defined(AMYBOARD) && !defined(AMYBOARD_WEB)
@@ -78,6 +79,12 @@ STATIC mp_obj_t tulip_midi_callback(size_t n_args, const mp_obj_t *args) {
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(tulip_midi_callback_obj, 1, 1, tulip_midi_callback);
+
+STATIC mp_obj_t tulip_external_midi_sync(size_t n_args, const mp_obj_t *args) {
+    amy_external_midi_sync(mp_obj_is_true(args[0]) ? 1 : 0);
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(tulip_external_midi_sync_obj, 1, 1, tulip_external_midi_sync);
 
 
 
@@ -1520,6 +1527,7 @@ STATIC const mp_rom_map_elem_t tulip_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_seq_remove_callback), MP_ROM_PTR(&tulip_seq_remove_callback_obj) },
     { MP_ROM_QSTR(MP_QSTR_seq_remove_callbacks), MP_ROM_PTR(&tulip_seq_remove_callbacks_obj) },
     { MP_ROM_QSTR(MP_QSTR_midi_callback), MP_ROM_PTR(&tulip_midi_callback_obj) },
+    { MP_ROM_QSTR(MP_QSTR_external_midi_sync), MP_ROM_PTR(&tulip_external_midi_sync_obj) },
 #ifndef __EMSCRIPTEN__
     { MP_ROM_QSTR(MP_QSTR_amy_block_done_callback), MP_ROM_PTR(&tulip_amy_block_done_callback_obj) },
     { MP_ROM_QSTR(MP_QSTR_amy_get_input_buffer), MP_ROM_PTR(&tulip_amy_get_input_buffer_obj) },
