ad887f237370cb6fe21fb8971421a3c9c81e067c
Dan Ellis <dan.ellis@gmail.com>
2026-03-10 10:17:44 -0400

    Update self_test.py to not expect loopbacks by default
    The loopback tests are still there, but will only run on self_test(True).

--- tulip/shared/py/self_test.py
+++ tulip/shared/py/self_test.py
@@ -14,9 +14,9 @@ def input_rms(blocks=10):
     rmss.append(np.sqrt(np.mean(waveform * waveform)))
   return float(np.mean(rmss))
 
-def self_test():
+def self_test(have_loopbacks=False):
   """Run a series of hardware self-tests."""
-  # This assumes that loop-back cables have been inserted between in and out
+  # have_loopbacks indicates that loop-back cables have been inserted between in and out
   # for line(audio), MIDI, CV1, CV2
   all_pass = []
 
@@ -35,29 +35,31 @@ def self_test():
   rms = input_rms()
   print("Quiescent input rms: %6.2f" % rms, " " * 8, 
         passfail(rms > 0.6 and rms < 2.0))
-  # 2. Check that audio-in is large when output is large
-  amy.send(osc=0, freq=440, vel=0.1)
-  time.sleep(0.1)
-  rms = input_rms()
-  print("Active    input rms: %6.2f" % rms, " " * 8,
-        passfail(rms > 80.0 and rms < 120.0))
-  # 3. Check MIDI loopback
-  midi.config.reset()
-  global rx_midi
-  rx_midi = []
-
-  def trap_midi(m):
+  if have_loopbacks:
+    # 2. Check that audio-in is large when output is large
+    amy.send(osc=0, freq=440, vel=0.1)
+    time.sleep(0.1)
+    rms = input_rms()
+    print("Active    input rms: %6.2f" % rms, " " * 8,
+          passfail(rms > 80.0 and rms < 120.0))
+    # 3. Check MIDI loopback
+    midi.config.reset()
     global rx_midi
-    rx_midi = [int(i) for i in m]
+    rx_midi = []
 
-  midi.add_callback(trap_midi)
-  time.sleep(0.1)
-  tulip.midi_out([0x90, 0x40, 0x7F])
-  time.sleep(0.1)
-  print("MIDI received: %-21s" % str([i for i in rx_midi]), 
-        passfail(rx_midi == [0x90, 0x40, 0x7F]))
-  midi.remove_callback(trap_midi)
-  # 4. Test CV loopback
+    def trap_midi(m):
+      global rx_midi
+      rx_midi = [int(i) for i in m]
+
+    midi.add_callback(trap_midi)
+    time.sleep(0.1)
+    tulip.midi_out([0x90, 0x40, 0x7F])
+    time.sleep(0.1)
+    print("MIDI received: %-21s" % str([i for i in rx_midi]), 
+          passfail(rx_midi == [0x90, 0x40, 0x7F]))
+    midi.remove_callback(trap_midi)
+
+  # 4. Test CV (incl. loopback)
   for channel in range(2):
     amyboard.cv_out(volts=0.0, channel=channel)
     time.sleep(0.1)
@@ -65,13 +67,14 @@ def self_test():
     print("CV%d volts_bias %5.2f" % (channel, volts_bias),
           " " * 15,
           passfail(volts_bias > -1.5 and volts_bias < 1.5))
-    for volts in [-5, 5]:
-      amyboard.cv_out(volts=volts, channel=channel)
-      time.sleep(0.1)
-      volts_in = amyboard.cv_in(channel)
-      print("CV%d volts_out %4.1f volts_in %5.2f" % (channel, volts, volts_in),
-            " " * 2,
-            passfail(abs(volts_in - volts_bias - volts) < 0.2))
+    if have_loopback:
+      for volts in [-5, 5]:
+        amyboard.cv_out(volts=volts, channel=channel)
+        time.sleep(0.1)
+        volts_in = amyboard.cv_in(channel)
+        print("CV%d volts_out %4.1f volts_in %5.2f" % (channel, volts, volts_in),
+              " " * 2,
+              passfail(abs(volts_in - volts_bias - volts) < 0.2))
   # Audible report
   if all(all_pass):
     # All OK chime
